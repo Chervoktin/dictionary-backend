39c8f52d9abe8a5e9201c1a6ce7a3c5c9e6265a4
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-01-21 13:44:36 +0300

    add models

--- words/models.py
+++ words/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Value(models.Model):
+    value = models.CharField(max_length=50, verbose_name='Значение')
+
+    class Meta:
+        verbose_name = 'Значение'
+        verbose_name_plural = 'Значения'
+
+    def __str__(self):
+        return self.value
+
+
+class Word(models.Model):
+    word = models.CharField(max_length=70, verbose_name='Слово')
+    value = models.ManyToManyField(Value, through='WordValue')
+
+    def __str__(self):
+        return self.word
+
+    class Meta:
+        verbose_name = 'Слово'
+        verbose_name_plural = 'Слова'
+
+
+class WordValue(models.Model):
+    word = models.ForeignKey(
+        Word, on_delete=models.CASCADE, verbose_name='Слово')
+    value = models.ForeignKey(
+        Value, on_delete=models.CASCADE, verbose_name='Значение')
+
+    class Meta:
+        verbose_name = 'Слово со значением'
+        verbose_name_plural = 'Слова со значением'
+
+    def __str__(self):
+        return self.word.word + ' - ' + self.value.value
+
+
+class Card(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.DO_NOTHING, verbose_name='Пользователь')
+    phrase = models.CharField(max_length=100, verbose_name='Фраза')
+    wordValue = models.ManyToManyField(WordValue, verbose_name='Слово')
+    media = models.FileField(upload_to='examples/', verbose_name='Пример')
+
+    class Meta:
+        verbose_name = 'Карточка'
+        verbose_name_plural = 'Карточки'
+
+    def __str__(self):
+        return self.phrase
