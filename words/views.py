9310083dc1e50c2f865f52e08d3615afc0554216
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-01-27 20:58:13 +0300

    changed views.py

--- words/views.py
+++ words/views.py
@@ -38,18 +38,42 @@ def auth(request):
             return JsonResponse(userForm.errors, status=400)
 
 
+class JWTUser():
+
+    def __init__(self, request):
+        self.request = request
+
+    def isExpired(self):
+        delta = datetime.now() - self.time
+        timeOfLife = timedelta(minutes=1)
+        return delta > timeOfLife
+
+    def isValid(self):
+        try:
+            encoded_jwt = self.request.META['HTTP_AUTHORIZATION']
+            self.user_id = jwt.decode(
+                encoded_jwt, JWT_KEY, algorithms=['HS256'])['id']
+            self.time = jwt.decode(encoded_jwt, JWT_KEY,
+                                   algorithms=['HS256'])['time']
+            self.time = datetime.strptime(self.time, '%Y-%m-%d %H:%M:%S.%f')
+            return True
+        except:
+            return False
+            
+    def getUser(self):
+        return User.objects.get(id=self.user_id)
+
+
 @csrf_exempt
 def getCards(request):
-    try:
-        encoded_jwt = request.META['HTTP_AUTHORIZATION']
-        user_id = jwt.decode(encoded_jwt, JWT_KEY, algorithms=['HS256'])['id']
-        time = jwt.decode(encoded_jwt, JWT_KEY, algorithms=['HS256'])['time']
-        time = datetime.strptime(time, '%Y-%m-%d %H:%M:%S.%f')
-        delta = datetime.now() - time
-        timeOfLife = timedelta(minutes=1)
-        if(delta > timeOfLife):
-            return JsonResponse({'error message': 'token expired'}, status=403)
+    jwtUser = JWTUser(request)
+    if jwtUser.isValid():
+        if not jwtUser.isExpired():
+            user = jwtUser.getUser()
+            return JsonResponse(list(Card.objects.filter(user=user).values()), safe=False)
         else:
-            return JsonResponse(list(Card.objects.filter(user_id=user_id).values()), safe=False)
-    except:
-        return JsonResponse({'error message': 'invaild token'}, status=403)
+            return JsonResponse({'error message': 'token expired'})
+    else:
+        return JsonResponse({'error message': 'token invaild'})
+
+    
