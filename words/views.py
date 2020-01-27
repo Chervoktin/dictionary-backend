2b78a03e57382c6e097c93be5c557324b53c0264
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-01-27 20:39:37 +0300

    change views.py

--- words/views.py
+++ words/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django import forms
+import jwt
+from dictionary.settings import JWT_KEY
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from words.models import Card
+from django.contrib.auth.models import User
+from datetime import datetime, timedelta
 
-# Create your views here.
+
+class UserForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    password = forms.CharField(max_length=100)
+
+
+@csrf_exempt
+def auth(request):
+    if request.method == 'POST':
+        userForm = UserForm(request.POST)
+        if userForm.is_valid():
+            user = authenticate(
+                username=userForm.cleaned_data['username'], password=userForm.cleaned_data['password'])
+            if user is not None:
+                if user.is_active:
+                    jwtStr = jwt.encode({'id': user.id,
+                                         'time': str(datetime.now())
+                                         }, JWT_KEY, algorithm='HS256')
+                    return JsonResponse(
+                        {'token': str(jwtStr.decode('utf-8'))}
+                    )
+                else:
+                    return JsonResponse(None)
+            else:
+                return JsonResponse({'errorMessage': 'User not found'}, status=404)
+        else:
+            return JsonResponse(userForm.errors, status=400)
