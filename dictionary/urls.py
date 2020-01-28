ce40c342a30ddfd769f4a17b034114b493ce7c65
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-01-28 14:41:31 +0300

    change urlds.py

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from words.views import auth, getCards
+from words.views import auth, getCards, getWords
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', auth),
-    path('cards/', getCards)
+    path('cards/', getCards),
+     url(r'^card/(\d*)/words/$',getWords),
 ]
