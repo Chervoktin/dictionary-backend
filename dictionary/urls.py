5fb23e34142874447c01c804689ea8832c3f1005
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-01-29 16:35:26 +0300

    change urls.py

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from words.views import auth, WordsView, CardsView
+from words.views import auth, WordsView, CardsView, WordView, WordViewFind
 from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', auth),
     path('cards/', CardsView.as_view()),
-     url(r'^card/(\d*)/words/$',WordsView.as_view()),
+    url(r'^card/(\d*)/words/$',WordsView.as_view()),
+    url(r'^word/(\d*)/', WordView.as_view()),
+    url(r'^word/([a-z\'\s]*)/', WordViewFind.as_view()),
 ]
