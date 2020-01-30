8e2528ac96ea60b56c75e42288bbf1e4932726ae
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-01-30 16:23:36 +0300

    change admin.py

--- words/admin.py
+++ words/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
 from words.models import *
 
-admin.site.register(Word)
-admin.site.register(Value)
-admin.site.register(WordValue)
 admin.site.register(Card)
-admin.site.register(CardWordValue)
+
