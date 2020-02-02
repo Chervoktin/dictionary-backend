5140b256fcff7ed7d968d3039f09be9fd56aa1c8
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-02-02 23:54:19 +0300

    change urls.py

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', auth),
     url(r'^card/$',CardView.as_view()),
+    url(r'^card/(\d*)/word/$', CardWordView.as_view()),
    # path('cards/', CardsView.as_view()),
    # url(r'^card/(\d*)/words/$',WordsView.as_view()),
    # url(r'^word/(\d*)/', WordView.as_view()),
