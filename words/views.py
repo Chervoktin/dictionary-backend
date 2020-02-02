30e78bf183e48f6c1a78d011e2d4401cc8273fbc
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-02-02 23:54:00 +0300

    change views.py

--- words/views.py
+++ words/views.py
@@ -12,6 +12,7 @@ from django.views.generic import View
 from django.forms import ModelForm
 from django.utils.decorators import method_decorator
 
+
 class UserForm(forms.Form):
     username = forms.CharField(max_length=100)
     password = forms.CharField(max_length=100)
@@ -65,33 +66,36 @@ class JWTUser():
     def getUser(self):
         return User.objects.get(id=self.user_id)
 
+
 class JWTMixin():
 
-    def dispatch(self,request,*args, **kwargs):
+    def dispatch(self, request, *args, **kwargs):
         self.jwtUser = JWTUser(request)
         if self.jwtUser.isValid():
             if not self.jwtUser.isExpired():
-                return super().dispatch(request,*args, **kwargs)
+                return super().dispatch(request, *args, **kwargs)
             else:
-                return JsonResponse({'error message': 'token expired'},status = 419)
+                return JsonResponse({'error message': 'token expired'}, status=419)
         else:
             return JsonResponse({'error message': 'token invaild'}, status=403)
 
     def get_user(self):
         return self.jwtUser.getUser()
 
+
 class CardForm(ModelForm):
     class Meta:
         model = Card
-        exclude =  ['user','scores', 'word']
+        exclude = ['user', 'scores', 'word']
+
 
 @method_decorator(csrf_exempt, name='dispatch')
 class CardView(JWTMixin, View):
 
     def post(self, request):
-        cardForm = CardForm(request.POST,request.FILES)
+        cardForm = CardForm(request.POST, request.FILES)
         if cardForm.is_valid():
-            card = cardForm.save(commit= False)
+            card = cardForm.save(commit=False)
             card.user = self.get_user()
             card.save()
             return HttpResponse(status=201)
@@ -99,4 +103,25 @@ class CardView(JWTMixin, View):
             return JsonResponse(cardForm.errors)
 
     def get(self, request):
-        return HttpResponse("", status = 404)
+        return HttpResponse("", status=404)
+
+
+class WordForm(ModelForm):
+    class Meta:
+        model = Word
+        exclude = ['translations']
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CardWordView(JWTMixin, View):
+
+    def post(self, request, card_id):
+        wordForm = WordForm(request.POST)
+        if wordForm.is_valid():
+            word = wordForm.save()
+            card = Card.objects.get(id=card_id)
+            wordsWithCard = WordsWithCard(word=word, card=card)
+            wordsWithCard.save()
+            return HttpResponse(status=201)
+        else:
+            return HttpResponse(status=404)
