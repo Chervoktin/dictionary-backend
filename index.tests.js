07d1e1d1fa2ddd6b62a608e57b7e919aadf8c506
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-06-19 02:09:10 +0300

    change index.tests.js

--- index.tests.js
+++ index.tests.js
@@ -2,23 +2,23 @@ let app = require("./index.js");
 
 it("should insert sentence 'the war was lost'", function () {
     let storage = new app.SentenceStorgae();
-    storage.insert("the war was lost");
-    var result = storage.getById(0).sentence;;
-    var expectedResult = "the war was lost";
+    let id = storage.insert("the war was lost");
+    let result = storage.getById(id).sentence;;
+    let expectedResult = "the war was lost";
     if (result !== expectedResult) {
         throw new Error(`Expected ${expectedResult}, but got ${result}`);
     }
 });
 
-it("should select sentence 'the war was lost'", function () {
+it("should select sentence 'the war was lost' and 'i like apples", function () {
     let storage = new app.SentenceStorgae();
     storage.insert("the war was lost");
     storage.insert("I like apples");
 
-    var result = storage.select(2)[0].sentence;
-    var expectedResult = "the war was lost";
-    var result2 = storage.select(2)[1].sentence;
-    var expectedResult2 = "I like apples";
+    let result = storage.select(2)[0].sentence;
+    let expectedResult = "the war was lost";
+    let result2 = storage.select(2)[1].sentence;
+    let expectedResult2 = "I like apples";
 
     if (result !== expectedResult) {
         throw new Error(`Expected ${expectedResult}, but got ${result}`);
